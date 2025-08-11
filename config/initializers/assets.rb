ff8cb1eece67986249c18b9ac6c51ba2b2d08315
devSebb <burke.sebastian1@gmail.com>
2025-08-10 21:20:11 -0500

    5 fix 2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,9 @@ Rails.application.config.assets.paths << Rails.root.join("app/javascript")
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w[ admin.js admin.css ]
+
+# Ensure all ESM modules under app/javascript are precompiled with their logical paths
+js_root = Rails.root.join("app/javascript")
+Rails.application.config.assets.precompile += Dir.glob(js_root.join("**/*.js")).map {
+  |path| Pathname.new(path).relative_path_from(js_root).to_s
+}
