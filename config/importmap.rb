18b0bd628d339cc6797a06c8149202345825f34e
devSebb <burke.sebastian1@gmail.com>
2025-08-10 20:17:33 -0500

    importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,15 +3,16 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers", preload: true
-pin_all_from "vendor/javascript/utils", under: "utils", preload: true
 
-# Pin individual JavaScript files 
-pin "hero_animations", to: "app/javascript/hero_animations.js"
-pin "specializations_animations", to: "app/javascript/specializations_animations.js"
-pin "experience_animations", to: "app/javascript/experience_animations.js"
-pin "path_animation", to: "app/javascript/path_animation.js"
-pin "resume_animations", to: "app/javascript/resume_animations.js"
-pin "projects_animations", to: "app/javascript/projects_animations.js"
-pin "smooth_scroll", to: "app/javascript/smooth_scroll.js"
-pin "gsap_setup", to: "app/javascript/gsap_setup.js"
+# Pin controllers directory
+pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Pin individual JavaScript files
+pin "hero_animations", to: "hero_animations.js"
+pin "specializations_animations", to: "specializations_animations.js"
+pin "experience_animations", to: "experience_animations.js"
+pin "path_animation", to: "path_animation.js"
+pin "resume_animations", to: "resume_animations.js"
+pin "projects_animations", to: "projects_animations.js"
+pin "smooth_scroll", to: "smooth_scroll.js"
+pin "gsap_setup", to: "gsap_setup.js"
