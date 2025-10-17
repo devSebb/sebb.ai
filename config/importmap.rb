5cadfa8dbc229a953b4d8c4a273612a276fd30b3
devSebb <burke.sebastian1@gmail.com>
2025-10-17 16:23:42 -0500

    animations two

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,15 +5,15 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 
 # Pin controllers directory
-pin "controllers", to: "controllers/index.js"
+pin "controllers", to: "controllers/index.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # Pin individual JavaScript files
-pin "hero_animations", to: "hero_animations.js"
-pin "specializations_animations", to: "specializations_animations.js"
-pin "experience_animations", to: "experience_animations.js"
-pin "path_animation", to: "path_animation.js"
-pin "resume_animations", to: "resume_animations.js"
-pin "projects_animations", to: "projects_animations.js"
-pin "smooth_scroll", to: "smooth_scroll.js"
-pin "gsap_setup", to: "gsap_setup.js"
+pin "hero_animations", to: "hero_animations.js", preload: true
+pin "specializations_animations", to: "specializations_animations.js", preload: true
+pin "experience_animations", to: "experience_animations.js", preload: true
+pin "path_animation", to: "path_animation.js", preload: true
+pin "resume_animations", to: "resume_animations.js", preload: true
+pin "projects_animations", to: "projects_animations.js", preload: true
+pin "smooth_scroll", to: "smooth_scroll.js", preload: true
+pin "gsap_setup", to: "gsap_setup.js", preload: true
